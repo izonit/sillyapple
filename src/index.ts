7155cfe0eecee11321bda7d460d4750e994688ab
izonit <daniilvorobyov0501@gmail.com>
2024-09-02 20:51:12 +0400

    delete: assets/ and add: yarn pm

--- src/index.ts
+++ src/index.ts
@@ -21,8 +21,8 @@ export const client = new SapphireClient({
         },
       ],
 
-      status: 'idle',
-    },
+    status: 'idle',
+  },
 
   loadMessageCommandListeners: true,
   defaultPrefix: defaultPrefix,
