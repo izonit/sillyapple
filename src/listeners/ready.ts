99824a7c76d90eca0c13a4ae03e4dc60c6482c85
izonit <daniilvorobyov0501@gmail.com>
2024-09-06 22:24:37 +0400

    change: Better console logging; "aesthetics".

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,4 +1,5 @@
 import { Listener } from "@sapphire/framework";
+import { cyan } from "colors";
 
 export class ReadyListener extends Listener {
   constructor(context, options) {
@@ -11,9 +12,10 @@ export class ReadyListener extends Listener {
 
   async run() {
     if (this.container.client && this.container.client.user) {
-      await new Promise(resolve => setTimeout(resolve, 1250));
+      await new Promise(resolve => setTimeout(resolve, 2550));
       // ^ Made because sometimes the message is sent inbetween application registry logs 
       console.log(this.container.logger.info(`Successfully logged in as ${this.container.client.user.tag}.`));
+      console.log(cyan(`</> DEVELOPMENT`));
     }
   }
 }
